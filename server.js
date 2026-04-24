fe64cbf2c587fab3a8c26106dbc04a36a546e817
Shiv <shivkumar121112@gmail.com>
2026-04-24 18:52:40 +0530

    fix cors origin

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const leadRoutes = require("./routes/leadRoutes");
 const agentRoutes = require("./routes/agentRoutes");
-const authRoutes = require("./routes/authRoutes"); // ✅ New
-const { errorHandler } = require("./middleware/errorMiddleware"); // ✅ New
+const authRoutes = require("./routes/authRoutes");
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
@@ -16,16 +16,15 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://anvaya-frontend-ejbqebbic-shivs-projects-5cdfdbed.vercel.app",
+      "https://anvaya-frontend-zeta.vercel.app", 
     ],
     credentials: true,
-  }),
+  })
 );
-// ✅ Duplicate app.use(cors()) hata diya — woh override kar raha tha upar wala
 
 app.use(express.json());
 
-app.use("/api/auth", authRoutes); // ✅ New
+app.use("/api/auth", authRoutes);
 app.use("/api/leads", leadRoutes);
 app.use("/api/agents", agentRoutes);
 
@@ -33,7 +32,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Anvaya CRM API is running!" });
 });
 
-app.use(errorHandler); // ✅ Sabse bottom pe, routes ke baad
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
